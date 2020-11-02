ae72d72f3bf4b6f5ce6bb24f550e0119e7c6c0fb
tamar <tamar5388@gmail.com>
2020-11-02 16:54:15 +0200

    jjj

---
+++ dotNet5781_01_8113_5037/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_8113_5037
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //jjj
+        }
+    }
+}
