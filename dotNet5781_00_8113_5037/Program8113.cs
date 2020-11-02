1978e4fc7c00cb3cfa20e2de20e55f536ae9c970
tamar <tamar5388@gmail.com>
2020-11-02 17:03:09 +0200

    two

---
+++ dotNet5781_00_8113_5037/Program8113.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_8113_5037
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8113();
+            Welcome5037();
+            Console.ReadKey();
+        }
+        static partial void Welcome5037();
+
+        private static void Welcome8113()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+
+        }
+    }
+
+}
--- dotNet5781_01_8113_5037/Program8113.cs
+++ dotNet5781_01_8113_5037/Program8113.cs
@@ -6,22 +6,8 @@ using System.Threading.Tasks;
 
 namespace dotNet5781_01_8113_5037
 {
-    partial class Program
+     class Program
     {
-        static void Main(string[] args)
-        {
-            Welcome8113();
-            Welcome5037();
-            Console.ReadKey();
-        }
-        static partial void Welcome5037();
-
-        private static void Welcome8113()
-        {
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("{0}, welcome to my first console application", name);
-
-        }
+       
     }
 }
