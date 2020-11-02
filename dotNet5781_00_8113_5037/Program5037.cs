ad55a12b15c37f108ecac10007310dc041df8f7b
r9822501 <r9822501@gmail.com>
2020-11-02 17:07:46 +0200

    VGSR

---
+++ dotNet5781_00_8113_5037/Program5037.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_8113_5037
+{
+    partial class Program
+    {
+        static partial void Welcome5037()
+        {
+            Console.WriteLine("I am also here");
+
+        }
+    }
+}
