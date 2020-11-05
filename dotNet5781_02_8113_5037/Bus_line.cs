b23c1d9d28156a260f8b74a9144783fc6d9da26f
tamar <tamar5388@gmail.com>
2020-11-05 21:07:26 +0200

    s

---
+++ dotNet5781_02_8113_5037/Bus_line.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_8113_5037
+{
+   class Bus_line:BusStation
+    {
+        int line_number;
+        //****************************************************
+        string Area = "";
+        //****************************************************
+
+        private BusStation first_station;
+
+        public BusStation Func_first_station
+        {
+            get { return first_station; }
+            set { first_station = value; }
+        }
+      
+        //****************************************************
+        private BusStation last_station;
+
+        public BusStation Func_last_station
+        {
+            get { return last_station; }
+            set { last_station = value; }
+        }
+        //****************************************************
+
+        private List<BusStation> Station;
+        public List<BusStation> Push_Bus_Station
+        {
+            get { return Station; }
+            set { Station = value; }
+        }
+        public void ToString()
+        {
+            Console.WriteLine($"Bus line: {this.line_number}\n Activity area: {this.Area}\n" +
+                $"Route:\n" );
+            Station.ForEach(Console.WriteLine($"{get.code}");)
+            
+
+           
+        }
+    }
+
+    
+
+}
---
+++ dotNet5781_02_8113_5037/Bus_line_station.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_8113_5037
+{
+    class Bus_line_station:BusStation
+    {
+        private float km_from_last_station;
+        public float func_km_from_last_station
+        {
+            get { return km_from_last_station; }
+            set { km_from_last_station = value; }
+        }
+
+        private TimeSpan time_from_last_station;
+        public TimeSpan func_time_from_last_station
+        {
+            get { return time_from_last_station; }
+            set { time_from_last_station = value; }
+        }
+    }
+        
+}
