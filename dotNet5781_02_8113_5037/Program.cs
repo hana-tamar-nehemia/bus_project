3b1c9e5a835fe6706d727626680cf7e18b0a37ff
r9822501 <r9822501@gmail.com>
2020-11-05 19:11:24 +0200

    תרגיל 2

---
+++ dotNet5781_02_8113_5037/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_8113_5037
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
