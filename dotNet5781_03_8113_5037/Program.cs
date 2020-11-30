b6039e0d54da8f1488e548b2bb04651fe03c5d79
tamar <tamar5388@gmail.com>
2020-11-30 18:41:08 +0200

    n

---
+++ dotNet5781_03_8113_5037/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_03_8113_5037
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
