1709f5871e5722e3f19ae2fc04030454d34ddfac
tamar <tamar5388@gmail.com>
2020-12-21 01:49:11 +0200

    צריך לבדוק למה כשתהליכון פועל אז אי אפשר לפתוח פרטים על אותו אוטובוס ובdatabus  למה בטקסט בוקס לא מופיע הפרטים על האוטובוס

--- dotNet5781_03B_8113_5037/AddBus.xaml.cs
+++ dotNet5781_03B_8113_5037/AddBus.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static dotNet5781_03B_8113_5037.MainWindow;
 
 namespace dotNet5781_03B_8113_5037
 {
@@ -19,24 +21,14 @@ namespace dotNet5781_03B_8113_5037
     /// </summary>
     public partial class AddBus : Window
     {
-        public AddBus()
+        Bus newbus;
+        public AddBus(ObservableCollection<Bus> buses)
         {
+            newbus = new Bus();
             InitializeComponent();
+            buses.Add(newbus);
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-
-            //System.Windows.Data.CollectionViewSource busViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("busViewSource")));
-            //// Load data by setting the CollectionViewSource.Source property:
-            //// busViewSource.Source = [generic data source]
-            //System.Windows.Data.CollectionViewSource busViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("busViewSource")));
-            //// Load data by setting the CollectionViewSource.Source property:
-            //// busViewSource.Source = [generic data source]
-            //System.Windows.Data.CollectionViewSource busViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("busViewSource")));
-            //// Load data by setting the CollectionViewSource.Source property:
-            //// busViewSource.Source = [generic data source]
-        }
 
         private void busListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -45,7 +37,76 @@ namespace dotNet5781_03B_8113_5037
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //כאן שהפרטים ישמרו ויוספו לרשימה של האטובוסים
+            if (newbus.lincegood(licens_plateTextBox.Text))
+                newbus.Licens_plate = licens_plateTextBox.Text;
+
+
+            string s = kmtreatTextBox.Text;
+            int a = int.Parse(s);
+
+            if (newbus.kmgood(kmtreatTextBox.Text))
+                newbus.Km = int.Parse(kmtreatTextBox.Text);
+
+            newbus.Date = (DateTime)dateDatePicker.SelectedDate;
+            newbus.Date_treatment = (DateTime)date_treatmentDatePicker.SelectedDate;
+
+            if (newbus.kmtreatgood(kmtreatTextBox.Text))
+                newbus.Km_between_treatment = int.Parse(kmtreatTextBox.Text); ;
+
+            if (int.Parse(kmtreatTextBox.Text) < 20000)
+                newbus.MyStatus = (Bus.Status)0;
+            else
+                newbus.MyStatus = (Bus.Status)3;
+
+            this.Close();
         }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource busViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("busViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // busViewSource.Source = [generic data source]
+            _ = ((System.Windows.Data.CollectionViewSource)(this.FindResource("busViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // busViewSource.Source = [generic data source]
+            _ = (System.Windows.Data.CollectionViewSource)(this.FindResource("busViewSource"));
+            // Load data by setting the CollectionViewSource.Source property:
+            // busViewSource.Source = [generic data source]
+        }
+
+        private void TextBox_OnlyNumbers_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+
+
+            TextBox text = sender as TextBox;
+            if (text == null) return;
+            if (e == null) return;
+
+            //allow get out of the text box
+            if (e.Key == Key.Enter || e.Key == Key.Return || e.Key == Key.Tab)
+                return;
+
+            //allow list of system keys (add other key here if you want to allow)
+            if (e.Key == Key.Escape || e.Key == Key.Back || e.Key == Key.Delete ||
+                e.Key == Key.CapsLock || e.Key == Key.LeftShift || e.Key == Key.Home
+             || e.Key == Key.End || e.Key == Key.Insert || e.Key == Key.Down || e.Key == Key.Right)
+                return;
+
+            char c = (char)KeyInterop.VirtualKeyFromKey(e.Key);
+
+            //allow control system keys
+            if (Char.IsControl(c)) return;
+
+            //allow digits (without Shift or Alt)
+            if (Char.IsDigit(c))
+                if (!(Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightAlt)))
+                    return; //let this key be written inside the textbox
+
+            //forbid letters and signs (#,$, %, ...)
+            e.Handled = true; //ignore this key. mark event as handled, will not be routed to other controls
+            return;
+        }
+
     }
+
 }
--- dotNet5781_03B_8113_5037/MainWindow.xaml.cs
+++ dotNet5781_03B_8113_5037/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace dotNet5781_03B_8113_5037
 {
@@ -20,25 +23,26 @@ namespace dotNet5781_03B_8113_5037
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<Bus> buses = new List<Bus>();
+        ObservableCollection<Bus> buses = new ObservableCollection<Bus>();
         public static Random r = new Random();
-        
+
 
         public MainWindow()
         {
             string str;
             for (int b = 1111111; b < 1111121; b++)
             {
-                int y=r.Next(5);
+                int y = r.Next(5);
                 str = b.ToString();
                 Bus a = new Bus(DateTime.Now, str);
                 buses.Add(a);
             }
             int i = 0;
-            buses[i++].km_between_treatment = 19999;
-            buses[i++].km = 1200;
+            buses[i++].Km_between_treatment = 19999;
+            buses[i++].Km = 1200;
             DateTime x = new DateTime(2010, 1, 3);
-            buses[i++].date_treatment = x;
+            buses[i].Date_treatment = x;
+            buses[i].MyStatus = (Bus.Status)3;
             listofbus = new ListBox();
             DataContext = buses;
 
@@ -48,22 +52,27 @@ namespace dotNet5781_03B_8113_5037
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-               MessageBox.Show(listofbus.SelectedItem.ToString());//חוזר סטרינג עם הפרטים
+            //MessageBox.Show(((Bus)listofbus.SelectedItem).ToString());
+            //Bus a = sender as Bus;
+            //MessageBox.Show(a.ToString());
+            databus a = new databus((Bus)listofbus.SelectedItem);
+            a.ShowDialog();
         }
 
 
         private void Button_Click(object sender, RoutedEventArgs e)//add
         {
             //כשילחצו יפתח החלון השני
-            AddBus newbus = new AddBus();
+
+            AddBus newbus = new AddBus(buses);
             newbus.ShowDialog();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Button_Click_1(object sender, RoutedEventArgs e)//תדלוק
         {
             Bus b = new Bus();
-            b= sender as Bus;
-            b.km = 0; //יש כאן בעיה
+            b = ((sender as Button).DataContext as Bus);
+            b.Km = 0;
             DataContext = b;
             MessageBox.Show("refueled");
         }
@@ -71,6 +80,16 @@ namespace dotNet5781_03B_8113_5037
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
 
+            Bus b = new Bus();
+            b = ((sender as Button).DataContext as Bus);
+            if (b.checkDate() == true)
+            {
+                travel a = new travel(b);
+                a.ShowDialog();
+            }
+            else
+                MessageBox.Show("refueled");
         }
     }
 }
+
