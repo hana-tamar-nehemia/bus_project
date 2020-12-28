cf3e72ac98bee06c71bd408330968f61f8d41afb
r9822501 <r9822501@gmail.com>
2020-12-28 22:20:09 +0200

    enum

--- BL/BO/Enums.cs
+++ BL/BO/Enums.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BO
 {
-    public enum PersonalStatus { SINGLE, MARRIED, DEVORSED, WIDOWER }
+    public enum Areas { Galilee, North, South, Jerusalem, Haifa, Krayot, Tiberias }
     public enum Bus_status { DRIVER, AVAILABLE_TRAVEL, TREATMENT, REFUELING }
     public enum StudentStatus { ACTIVE, SUSPENDED, ACADEMIC_VACATION, FINISHED }
     public enum StudentGraduate { BSC, MSC, PHD, BA, MA, MD }
--- DLAPI/DO/Enums.cs
+++ DLAPI/DO/Enums.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DO
 {
-    public enum PersonalStatus { SINGLE, MARRIED, DEVORSED, WIDOWER }
+    public enum Areas { Galilee, North, South, Jerusalem, Haifa, Krayot, Tiberias }
     public enum Bus_status { DRIVER, AVAILABLE_TRAVEL,TREATMENT , REFUELING}
     public enum StudentStatus { ACTIVE, SUSPENDED, ACADEMIC_VACATION, FINISHED }
     public enum StudentGraduate {  BSC, MSC, PHD, BA, MA, MD }
